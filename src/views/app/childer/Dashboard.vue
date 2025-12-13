13a69405c352bd754cbe53ac24f1550e252f5e87
Jamespatrick30 <mrsniper1388@gmail.com>
2025-12-13 12:57:34 +0800

    add the dashboard

--- src/views/app/childer/Dashboard.vue
+++ src/views/app/childer/Dashboard.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1>welcome to dashboard</h1>
+    <div class="w-full h-full">
+        <h1 class=" text-3xl font-bold p-5">Dashboard</h1>
+        <section class="w-full flex justify-center items-center h-fit" id="cards">
+            <div class=" w-[70%] flex justify-between items-center">
+                <div class=" bg-white shadow-md rounded-lg p-5 w-1/3 mx-2">
+                    <h2 class=" text-xl font-semibold mb-3">Total Graves</h2>
+                    <p class=" text-3xl font-bold">150</p>
+                </div>
+                <div class=" bg-white shadow-md rounded-lg p-5 w-1/3 mx-2">
+                    <h2 class=" text-xl font-semibold mb-3">Occupied Graves</h2>
+                    <p class=" text-3xl font-bold">90</p>
+                </div>
+                <div class=" bg-white shadow-md rounded-lg p-5 w-1/3 mx-2">
+                    <h2 class=" text-xl font-semibold mb-3">Available Graves</h2>
+                    <p class=" text-3xl font-bold">60</p>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
